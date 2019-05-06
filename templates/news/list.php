9be5c7706989ecad1ad41876ffa6032f31f0d7e2
gameisland <gameisland.y.uemura@gmail.com>
2019-05-06 22:27:13 +0900

    fix a bug

--- templates/news/list.php
+++ templates/news/list.php
@@ -4,7 +4,6 @@
 	<meta NAME=”ROBOTS” CONTENT=”NONE”>
 	<title>GOS2 | ニュース</title>
 	<link rel="stylesheet" type="text/css" href="/css/news.css<?php echo '?'.time() ?>">
-	<script src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
 </head>
 <body>
 	<div class="wrapper">
@@ -12,7 +11,7 @@
 		<div class="main_contents">
 			<div class="main_contents_wrapper">
 				<div class="breadcrumbs">
-					<p>トップ　＞　ニュース</p>
+					<p><a href="/">トップ</a>　＞　<a href="/news">ニュース</a></p>
 				</div>
 				<iframe src="//cms.gameisland.co.jp/gos2/<?php echo $platform; ?>/news_list_page.html" allowtransparency="true" frameborder="0" scrolling="no" style="display: block;height: 1500px;width: 1280px;"></iframe>
 			</div>
