bfa7403fe233d9be4afe0299c227fc2ef74b6793
YangLiangYong <yang-li@kingsoft.jp>
2019-08-01 15:49:45 +0900

    add commerce-2

---
+++ templates/support/law2.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta NAME=”ROBOTS” CONTENT=”NONE”>
+	<title>資金決済法に基づく表示</title>
+	<link rel="stylesheet" type="text/css" href="/css/support.css">
+</head>
+<body>
+
+	<?php if(PLATFORM == 'dmm') { include(__DIR__ . '/../adTag/dmm_gtm.php'); } ?>
+
+	<div class="wrapper">
+		<img src="/images/title_law2.png" width="100%">
+		<div>
+			<p>資金決済法に基づく表示</p>
+<!--			<br>-->
+<!--			<p>当社のアプリケーション、コンテンツ等(以下「本サービス」)に関する取引条件は、以下のとおりとなります。</p>-->
+			<br>
+			<br>
+			<p>■前払式支払手段の名称</p>
+			<p>ダイヤ</p>
+			<br>
+            <p>■配信会社</p>
+            <p>ACCESSPORT株式会社</p>
+            <br>
+			<p>■支払可能金額等</p>
+			<p>支払可能金額への制限は設けておりません。</p>
+            <p>但し『Guns of Soul2』利用規約の約款に準拠いたします。</p>
+            <br/>
+            <p>■有効期間または期限</p>
+            <p>有効期限はありません。</p>
+            <br/>
+            <p>■お問い合わせは下記のアドレスまでお願い致します</p>
+            <p>e-mail :identityv-jp@service.netease.com</p>
+            <br/>
+            <p>■ご利用上の注意</p>
+            <p>・ゲーム内通貨の表示価格は、税込価格となっております。</p>
+            <p>・ゲーム内通貨購入手続完了後のお客様のご都合によるご購入のキャンセルはお受けできません。</p>
+            <p>・その他ご利用上の注意事項については、利用規約をご確認ください。</p>
+            <br/>
+            <p>■使用場所</p>
+            <p>「Guns of Soul2」ゲーム内でのみ、ご利用いただけます。</p>
+            <br/>
+            <p>■未使用残高の確認方法</p>
+            <p>ゲーム内に表示されているゲーム内通貨の表示にて、その保有数が確認できます。</p>
+            <br/>
+            <p>■利用規約</p>
+            <p>その他ゲーム内通貨の利用に関しては、利用規約をご覧ください</p>
+            <br/>
+            <br/>
+		</div>
+
+		<?php include(__DIR__ . '/../vendor/backToTopBtn.php') ?>
+	</div>
+	<?php include(__DIR__ . '/../vendor/footer.php') ?>
+</body>
+</html>
